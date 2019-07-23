762ebec2e94ff708fe8e67f72e9843a83ad995c9
Aidan <aidansoftwaredev@gmail.com>
2019-07-23 23:16:35 +0100

    adding stuff

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './App.scss';
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        f
+      </header>
+    </div>
+  );
+}
+
+export default App;
