240b070ecea40bed8d2ac90dc1e136a93667c75b
Aidan <aidansoftwaredev@gmail.com>
2019-07-24 22:19:24 +0100

    ADding more content

---
+++ src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from 'Card.scss'
+
+const Card = props => {
+  
+  return (
+    <div className={styles.CardWrapper}>
+      {props.content}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  content: PropTypes.string
+}
+
+export default Card;
