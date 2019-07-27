10785a52e6c1e11afacf8454c7d856c6709d58f4
Aidan <aidansoftwaredev@gmail.com>
2019-07-27 20:30:37 +0100

    Updated burger and text

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -9,7 +9,7 @@ const Projects = props => {
   return (
     <div>
       <p>
-        Developing websites/web applications is more than just a carear, but a hobby. Out of office hours, I have worked on various projects which I will be going through the technologies used to develop said projects and a brief description of what the projects are
+        Developing websites/web applications is more than just a carear, but a hobby too. I have worked on various projects which I will be going through the technologies used to develop said projects.
       </p>
       <Card
         title="Burger Builder"
