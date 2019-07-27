a9926f7b5564f54ca1e13236ed834d91991bac4a
Aidan <aidansoftwaredev@gmail.com>
2019-07-27 12:31:10 +0100

    Icons

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import NavigationLink from './NavigationLink/NavigationLink'
+import Icon from '@mdi/react'
+import { mdiCodeBraces } from '@mdi/js'
+import { mdiTrophy } from '@mdi/js'
 
 import styles from './Style'
 
 const Navigation = props => {
-
+  console.log(props);
+  console.log(window.location)
   return (
     <div className={styles.navBar}>
-      <ul>
-        <NavigationLink link="/" exact>Projects</NavigationLink>
-        <NavigationLink link="/achievements" exact>Achievements</NavigationLink>
+      <ul className={styles.linkContainer}>
+        <NavigationLink
+          link="/"
+          exact >
+            <Icon
+              path={mdiCodeBraces}
+              size={1}
+              color='#ccc'
+            />
+              Projects
+        </NavigationLink>
+        <NavigationLink
+          link="/achievements"
+          exact>
+            <Icon
+              path={mdiTrophy}
+              size={1}
+            />
+          Achievements
+        </NavigationLink>
       </ul>
     </div>
   )
