9ba903933fbcf674b457b4a942e61e605a141df3
Aidan <aidansoftwaredev@gmail.com>
2019-07-28 12:00:42 +0100

    Card Styles

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -8,58 +8,31 @@ import styles from './Style'
 const Projects = props => {
 
   return (
-    <div className={styles.projectsWrapper}>
-      {Descriptions.projectsDescription}
-      <Card
-        title="Burger Builder"
-        content={Descriptions.burgerBuilderDesc}
-        img={BurgerBuilderImg}
-        imgLink
-        imgWidth={300}
-        imgHeight={200}
-        imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
-        imgAlt="Burger builder project"
-      />
-            <Card
-        title="Burger Builder"
-        content={Descriptions.burgerBuilderDesc}
-        img={BurgerBuilderImg}
-        imgLink
-        imgWidth={300}
-        imgHeight={200}
-        imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
-        imgAlt="Burger builder project"
-      />
-            <Card
-        title="Burger Builder"
-        content={Descriptions.burgerBuilderDesc}
-        img={BurgerBuilderImg}
-        imgLink
-        imgWidth={300}
-        imgHeight={200}
-        imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
-        imgAlt="Burger builder project"
-      />
-            <Card
-        title="Burger Builder"
-        content={Descriptions.burgerBuilderDesc}
-        img={BurgerBuilderImg}
-        imgLink
-        imgWidth={300}
-        imgHeight={200}
-        imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
-        imgAlt="Burger builder project"
-      />
-            <Card
-        title="Burger Builder"
-        content={Descriptions.burgerBuilderDesc}
-        img={BurgerBuilderImg}
-        imgLink
-        imgWidth={300}
-        imgHeight={200}
-        imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
-        imgAlt="Burger builder project"
-      />
+    <div>
+       {Descriptions.projectsDescription}
+      <div className={styles.projectCards}>
+      
+        <Card
+          title="Burger Builder"
+          content={Descriptions.burgerBuilderDesc}
+          img={BurgerBuilderImg}
+          imgLink
+          imgWidth={300}
+          imgHeight={200}
+          imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
+          imgAlt="Burger builder project"
+        />
+        <Card
+          title="Burger Builder"
+          content={Descriptions.burgerBuilderDesc}
+          img={BurgerBuilderImg}
+          imgLink
+          imgWidth={300}
+          imgHeight={200}
+          imgLinkLocation="https://aidans-burger-builder.firebaseapp.com/"
+          imgAlt="Burger builder project"
+        />
+      </div>
     </div>
   )
 }
