957f132009ebfee73e135b57cd3579cb0c1ef01a
Aidan <aidansoftwaredev@gmail.com>
2019-08-24 00:22:38 +0100

    ADding skills content

--- src/containers/Achievements/Achievements.js
+++ src/containers/Achievements/Achievements.js
@@ -11,12 +11,12 @@ const Achievements = props => {
       {/* {descriptions.achivementDescription} */}
     <h1>Skills</h1>
       <div className={styles.skillsContainer}>
-        <h2>JavaScript</h2>
-        <h2>React</h2>
-        <h2>Angular</h2>
-        <h2>Node.js</h2>
-        <h2>MySQL</h2>
-        <h2>AWS</h2>
+        <div><h2>JavaScript</h2><p>From front-end frameworks to using JQuery to vanilla JavaScript when writting my first web applications. JavaScript has been a constant in my web development carear.</p></div>
+        <div><h2>React</h2><p>The front-end framework I use within my organisation to create CMS, Callback Solutions and web applications</p></div>
+        <div><h2>Angular</h2><p>Developed public facing forms for finacial(check spelling) services using this framework. Creating a front-end that looks good both on 300px wide mobiles and 1920px desktops</p></div>
+        <div><h2>Node.js</h2><p>Used in conjunction with <code>Koa</code> and <code>Express</code> to server secure services.</p></div>
+        <div><h2>MySQL</h2><p>The RDMS used for persitant data storage, experiance in both Oracle database engine and MS SQL creating transactional SQL procedures.</p></div>
+        <div><h2>AWS</h2><p>The cloud platform used to host this portfolio and my organisations mission criticle applications</p></div>
       </div>
     <h1>Certifications</h1>
     <div className={styles.achCards}>
