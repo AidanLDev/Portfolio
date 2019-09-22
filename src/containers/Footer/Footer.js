2403ceb146d2229ef02aa5f7682bcf8a96af0022
Aidan <aidansoftwaredev@gmail.com>
2019-09-22 17:29:16 +0100

    Footer refactor and SSL img added

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import Button from 'components/Button/Button'
-import Icon from '@mdi/react';
-import { mdiChevronUp } from '@mdi/js'
-import LinkedinLogo from 'assets/LinkedinLogo.png'
+import React from "react";
+import Button from "components/Button/Button";
+import Icon from "@mdi/react";
+import { mdiChevronUp } from "@mdi/js";
+import LinkedinLogo from "assets/LinkedinLogo.png";
+import sslImg from "assets/positivessl_trust_seal_md_167x42.png";
 
-import styles from './Style'
+import styles from "./Style";
 
 const Footer = props => {
-
   const backToTop = () => window.scrollTo(0, 0);
 
   return (
     <div className={styles.footerWrapper}>
-      <a href="https://www.linkedin.com/in/aidanlowson1/" rel="noopener noreferrer" target="_blank">
-        <img src={LinkedinLogo} alt="LinkedIn Logo" width="50" height="50"/>
+      <img
+        src={sslImg}
+        alt="SSL Certificate"
+        className={styles.sslImg}
+        width="167"
+        height="42"
+      />
+      <a
+        href="https://www.linkedin.com/in/aidanlowson1/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img src={LinkedinLogo} alt="LinkedIn Logo" width="50" height="50" />
       </a>
-      <div
-        onClick={backToTop}
-        className={styles.backToTopWrapper}>
-        <Button>
-          Back to top
-        </Button>
-        <Icon
-          path={mdiChevronUp}
-          size={1}
-          color={'white'}
-        />
+      <div onClick={backToTop} className={styles.backToTopWrapper}>
+        <Button>Back to top</Button>
+        <Icon path={mdiChevronUp} size={1} color={"white"} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
