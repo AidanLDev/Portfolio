76586c87f447827ea0cf977dd380c752fdb9f329
Aidan <aidanlowson@gmail.com>
2019-09-23 21:06:30 +0100

    Adding HTTPS options and serving https server

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const path = require("path");
-const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
 const fs = require("fs");
+const http = require("http");
 const https = require("https");
 
 const cert = fs.readFileSync(
@@ -19,43 +18,37 @@ const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 443;
 const hostname = "aidanlowson.com";
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const app = express();
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+const httpsOptions = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
-    );
-  });
-} else {
-  const app = express();
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+// Answer API requests.
+app.get("/api", function(req, res) {
+  res.set("Content-Type", "application/json");
+});
 
-  // Answer API requests.
-  app.get("/api", function(req, res) {
-    res.set("Content-Type", "application/json");
-  });
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+});
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get("*", function(request, response) {
-    response.sendFile(
-      path.resolve(__dirname, "../frontend/build", "index.html")
-    );
-  });
+https
+  .createServer(httpsOptions, app)
+  .listen(PORT, () =>
+    console.log(`listening on port https://localhost:${PORT}`)
+  );
 
-  app.listen(PORT, function() {
-    console.error(
-      `Node ${
-        isDev ? "dev server" : "cluster worker " + process.pid
-      }: listening on port http://localhost:${PORT}`
-    );
-  });
-}
+// app.listen(PORT, function() {
+//   console.error(
+//     `Node ${
+//       isDev ? "dev server" : "cluster worker " + process.pid
+//     }: listening on port http://localhost:${PORT}`
+//   );
+// });
