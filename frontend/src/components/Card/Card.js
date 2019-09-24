0fb8b3ad15a94c5605d78fcc64414432524ab98e
Aidan Lowson <aidan.lowson@contactpartners.com>
2019-09-24 15:03:09 +0100

    Card tidy up

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,29 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import Tooltip from 'components/Tooltip/Tooltip'
 
 import styles from "./Style";
 
 const Card = props => {
-  // const [active, setActive] = useState(false);
-  // const [mousePosition, setMousePosition] = useState(null);
-
-  // const handleMouseOver = e => {
-  //   setTimeout(function(){setActive(true)}, 1200)
-  //   setMousePosition([e.pageX, e.pageY])
-  // }
-  // const handleMouseLeave = () => setActive(false);
-
   return (
-    <div className={styles.CardWrapper} /* onMouseLeave={handleMouseLeave} */>
-      {/* <Tooltip text={`Click to navigate to the ${props.imgAlt}`} active={active} position={mousePosition}/> */}
+    <div className={styles.CardWrapper}>
       <h2>{props.title}</h2>
       {props.imgLink ? (
         <a
           href={props.imgLinkLocation}
           rel="noopener noreferrer"
           target="_blank"
-          // onMouseLeave={handleMouseLeave}
         >
           <img
             src={props.img}
@@ -31,8 +19,6 @@ const Card = props => {
             width={props.imgWidth}
             height={props.imgHeight}
             className={props.transition ? styles.transitionStyle : null}
-            // onMouseOver={e => handleMouseOver(e)}
-            // onMouseLeave={handleMouseLeave}
           />
         </a>
       ) : (
