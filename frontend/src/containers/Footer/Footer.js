9395294ff22eadecdfcc3aeb6cad7d472dae8e1a
Aidan Lowson <aidan.lowson@contactpartners.com>
2019-09-24 15:02:03 +0100

    Tooltip!

--- frontend/src/containers/Footer/Footer.js
+++ frontend/src/containers/Footer/Footer.js
@@ -16,13 +16,17 @@ const Footer = props => {
 
   return (
     <div className={styles.footerWrapper}>
-      <img
-        src={sslImg}
-        alt="SSL Certificate"
-        className={styles.sslImg}
-        width="167"
-        height="42"
-      />
+      <div className={styles.tooltip}>
+        <span className={styles.tooltiptext}>Secure SSL Certificate</span>
+        <img
+          src={sslImg}
+          alt="SSL Certificate"
+          width="167"
+          height="42"
+        />
+      </div>
+      <div className={styles.tooltip}>
+        <span className={styles.tooltiptext}>Linkedin Profile</span>
         <a
           href="https://www.linkedin.com/in/aidanlowson1/"
           rel="noopener noreferrer"
@@ -36,6 +40,7 @@ const Footer = props => {
             whileHover={{ scale: hoverScale }}
             whileTap={{ scale: tapScale }} />
         </a>
+      </div>
       <motion.div onClick={backToTop} className={styles.backToTopWrapper} whileHover={{ scale: hoverScale }} whileTap={{ scale: tapScale }}>
         <Button>Back to top</Button>
         <Icon path={mdiChevronUp} size={1} color={"white"} />
