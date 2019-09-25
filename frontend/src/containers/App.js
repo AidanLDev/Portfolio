83f2836476d5294a72f941913db982af35d6067d
Aidan Lowson <aidan.lowson@contactpartners.com>
2019-09-25 16:57:58 +0100

    responsive navbar

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
+import { mdiMenu } from '@mdi/js';
+import Icon from '@mdi/react';
 
 import Navigation from 'containers/Navigation/Navigation'
 import Achievements from 'containers/Achievements/Achievements'
@@ -7,8 +9,11 @@ import Projects from 'containers/Projects/Projects'
 import Footer from 'containers/Footer/Footer'
 
 import styles from './Style'
+import Sidedrawer from './Navigation/Sidedrawer/Sidedrawer'
 
 const App = () => {
+
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
 
   const routes = (
     <Switch>
@@ -18,11 +23,24 @@ const App = () => {
     </Switch>
   )
 
+  const closeSideDrawer = () => setShowSideDrawer(false);
+  const openSideDrawer = () => setShowSideDrawer(true);
+
   return (
-    <div className={styles.App}>  
-    {/* TODO: Background img  */}
+    <div className={styles.App}>
       <header className={styles.AppHeader}>
-        <Navigation />
+        <nav className={styles.desktopOnly}>
+          <Navigation />
+        </nav>
+        <nav>
+          <div className={styles.mobileOnly}>
+            <Icon path={mdiMenu} size={2} color={'white'} onClick={openSideDrawer}/>
+            {/* Logo */}
+          </div>
+          {showSideDrawer &&
+            <Sidedrawer showDrawer={showSideDrawer} closeDrawer={closeSideDrawer} />
+          }
+        </nav>
       </header>
       <div className={styles.MainContent}>
         {routes}
---
+++ frontend/src/containers/Navigation/Sidedrawer/Sidedrawer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Backdrop from 'components/Backdrop/Backdrop';
+import Button from 'components/Button/Button'
+import Navigation from 'containers/Navigation/Navigation';
+
+import styles from './Style.scss';
+
+const sidedrawer = props => {
+  return (
+    <div>
+      <Backdrop show={props.showDrawer} clicked={props.closeDrawer}/>
+      <div className={styles.sidedrawer}>
+        <div>Menu</div>
+        {/* Logo */}
+        <nav>
+          <Navigation />
+        </nav>
+        <Button clicked={props.closeDrawer}>Close Menu</Button>
+      </div>
+    </div>
+  );
+}
+
+export default sidedrawer;
