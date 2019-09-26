4b20c74d299cf516048646a74c067bb9fdb1b5df
Aidan <aidansoftwaredev@gmail.com>
2019-09-26 20:05:48 +0100

    ADding styles to nav

--- frontend/src/containers/Navigation/NavigationLink/NavigationLink.js
+++ frontend/src/containers/Navigation/NavigationLink/NavigationLink.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { motion } from 'framer-motion'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 
-import styles from './Style'
-import Icon from '@mdi/react';
+import styles from "./Style";
+import Icon from "@mdi/react";
 
 const NavigationLink = props => (
-  <motion.li className={styles.NavLink} whileTap={{ scale: 0.9 }}>
+  <motion.li
+    className={styles.NavLink}
+    whileTap={{ scale: 0.9 }}
+    whileHover={{ scale: 1.2 }}
+  >
     <NavLink
       to={props.link}
       exact={props.exact}
       activeClassName={styles.active}
     >
       {props.children}
-      {props.iconPath
-        ? <Icon
-        path={props.iconPath}
-        size={1.2}
-        color={'#faebd7'}
-      />
-        : null}
+      {props.iconPath ? (
+        <Icon path={props.iconPath} size={1.2} color={"#faebd7"} />
+      ) : null}
     </NavLink>
   </motion.li>
-)
+);
 
 export default NavigationLink;
