3ba55fccb6e0434a222010da9a62f788e663dfe9
Aidan <aidansoftwaredev@gmail.com>
2019-09-30 21:11:57 +0100

    slight ammendment

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.get("/api", function(req, res) {
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get("*", function(request, response) {
+app.get("/", function(request, response) {
   response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
