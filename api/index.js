595ebfb7ca9027f8af4a0fb3f5cc4079bf86e62c
Aidan <aidanlowson@gmail.com>
2019-09-30 18:03:03 +0100

    Test

--- api/index.js
+++ api/index.js
@@ -4,11 +4,42 @@ const fs = require("fs");
 const http = require("http");
 const https = require("https");
 
-const isDev = process.env.NODE_ENV !== "production";
+const cert = fs.readFileSync(
+  path.join(__dirname, "./certs", "/aidanlowson_com.crt")
+);
+const ca = fs.readFileSync(
+  path.join(__dirname, "./certs", "/aidanlowson_com.ca-bundle")
+);
+const key = fs.readFileSync(
+  path.join(__dirname, "./certs", "/aidanlowson.key")
+);
+
 const PORT = process.env.PORT || 5000;
-// const hostname = "aidanlowson.com";
 
 const app = express();
+
+//  X-Forwarded-Proto header field to be trusted so its value can be used to determine the portocol.
+app.enable("trust proxy");
+
+app.use(function(req, res, next) {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
+
+const httpsOptions = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
+
+https
+  .createServer(options, function(req, res) {
+    res.end("Secure");
+  })
+  .listen(443);
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
@@ -23,50 +54,8 @@ app.get("*", function(request, response) {
   response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
-http.get("*", (req, res, next) => {
-  if (req.protocol === "http") {
-    res.redirect(res.redirect("https://" + req.headers.host + req.url));
-    //  'https://aidanlowson.com'
-  }
-});
-
 http
-  .createServer(app)
+  .createServer(httpsOptions, app)
   .listen(PORT, () =>
     console.log(`listening on port https://localhost:${PORT}`)
   );
-
-// const cert = fs.readFileSync(
-//   path.join(__dirname, "./certs", "/aidanlowson_com.crt")
-// );
-// const ca = fs.readFileSync(
-//   path.join(__dirname, "./certs", "/aidanlowson_com.ca-bundle")
-// );
-// const key = fs.readFileSync(
-//   path.join(__dirname, "./certs", "/aidanlowson.key")
-// );
-
-// const httpsOptions = {
-//   key: key,
-//   cert: cert,
-//   ca: ca
-// };
-
-/* simple redirect middleware  */
-// app.use(function(req, res, next) {
-//   if (req.secure) {
-//     // request was via https, so do no special handling
-//     next();
-//   } else {
-//     // request was via http, so redirect to https
-//     res.redirect("https://" + req.headers.host + req.url);
-//   }
-// });
-
-//  Redirect?
-// if (req.protocol === 'http') {
-//   http.get('*', function(req, res) {
-//     res.redirect('https://' + req.headers.host + req.url);
-//     //  'https://aidanlowson.com'
-//   });
-// }
