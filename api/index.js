936ac66c207d1b437b41c63ee5e860323521e527
Aidan <aidansoftwaredev@gmail.com>
2019-10-01 19:27:33 +0100

    https

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const http = require("http");
+const https = require("https");
 const PORT = 443;
 
 const app = express();
@@ -40,7 +40,7 @@ app.get("*", function(req, res) {
 
 //  Use proxy to send traffic from http -> https
 
-http
+https
   .createServer(httpsOptions, app)
   .listen(PORT, () =>
     console.log(`listening on port http://localhost:${PORT}`)
