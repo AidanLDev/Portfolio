d8ee9a9cd83c9ffa33bc6f78d397b25dac41e2d4
Aidan <aidansoftwaredev@gmail.com>
2019-10-12 19:45:48 +0100

    Filtering!

--- frontend/src/containers/Blog/BlogNav/BlogNav.js
+++ frontend/src/containers/Blog/BlogNav/BlogNav.js
@@ -1,49 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import BlogCard from "components/BlogCard/BlogCard";
 import * as blogData from "data/Blog/blogData";
 
 import styles from "./Style";
 
 const BlogNav = () => {
+  const [filterValue, setFilterValue] = useState("");
+  const handleInputChange = event => setFilterValue(event.target.value);
+
+  const blogs = [
+    {
+      title: "Deploying your first website",
+      description: blogData.deployingWebAppDesc,
+      route: "deploying-a-website"
+    },
+    {
+      title: "# Must use JavaScript array methods",
+      description: blogData.placeHolder1,
+      route: "must-use-array-methods"
+    },
+    {
+      title: "Reacts useEffect()",
+      description: blogData.placeHolder2,
+      route: "using-use-effect"
+    },
+    {
+      title: "Will Quantum Computing break encryption",
+      description: blogData.placeHolder3,
+      route: "quantum-computing"
+    },
+    {
+      title: "JS ES6 using Spread vs Flatten",
+      description: blogData.placeHolder3,
+      route: "spread-vs-flatten"
+    },
+    {
+      title: "JavaScript prototype",
+      description: blogData.placeHolder3,
+      route: "javascript-prototype"
+    }
+  ];
+
   return (
     <div className={styles.container}>
       <div className={styles.rightSidebar}>
         <span>Search</span>
         {/* Make a nice UI component */}
-        <input type="text" name="search"></input>
+        <input
+          name="search"
+          placeholder="Search Blogs"
+          value={filterValue}
+          onChange={handleInputChange}
+        />
       </div>
       <div className={styles.mainContainer}>
         <h1>Blog Posts</h1>
-        <BlogCard
-          title="Deploying your first website"
-          description={blogData.deployingWebAppDesc}
-          route="deploying-a-website"
-        />
-        <BlogCard
-          title="# Must use JavaScript array methods"
-          description={blogData.placeHolder1}
-          route="must-use-array-methods"
-        />
-        <BlogCard
-          title="Reacts useEffect()"
-          description={blogData.placeHolder2}
-          route="using-use-effect"
-        />
-        <BlogCard
-          title="Will Quantum Computing break encryption"
-          description={blogData.placeHolder3}
-          route="quantum-computing"
-        />
-        <BlogCard
-          title="JS ES6 using Spread vs Flatten"
-          description={blogData.placeHolder3}
-          route="spread-vs-flatten"
-        />
-        <BlogCard
-          title="JavaScript prototype"
-          description={blogData.placeHolder3}
-          route="javascript-prototype"
-        />
+        {blogs.map(blog => {
+          if (filterValue === "" || filterValue === blog.title) {
+            return (
+              <BlogCard
+                key={blog.title}
+                title={blog.title}
+                description={blog.description}
+                route={blog.route}
+              />
+            );
+          }
+          return null;
+        })}
       </div>
     </div>
   );
