c812a3a49e309c9c900620897a228608b08ba850
Aidan <aidansoftwaredev@gmail.com>
2020-03-23 19:28:51 +0000

    Updating bucket to serve hosted site :S

--- frontend-old/src/blogs/index.js
+++
@@ -1,8 +0,0 @@
-export { deployWebsite } from "./Tech/01-deploying-to-the-web";
-export {
-  javaScriptArrayMethods
-} from "./Tech/02-must-use-javascript-array-methods";
-export { usingUseEffect } from "./Tech/03-using-use-effect";
-export { quantumComputing } from "./Tech/04-quantum-computing";
-export { spreadFlatten } from "./Tech/05-spread-vs-flatten";
-export { javaScriptProtoType } from "./Tech/06-javascipt-prototype";
