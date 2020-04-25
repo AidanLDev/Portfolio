d72b50fd8f66b76049f98e62682019e7bf86f08d
Aidan <aidansoftwaredev@gmail.com>
2020-04-25 16:26:59 +0100

    Adding github to toolbar

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { motion } from "framer-motion";
-import Button from "components/Button/Button";
-import EmailIcon from "@material-ui/icons/Email";
-import Icon from "@mdi/react";
-import { mdiChevronUp } from "@mdi/js";
-import LinkedinLogo from "assets/LinkedinLogo.png";
+import React from 'react';
+import { motion } from 'framer-motion';
+import Button from 'components/Button/Button';
+import EmailIcon from '@material-ui/icons/Email';
+import Icon from '@mdi/react';
+import { mdiChevronUp } from '@mdi/js';
+import LinkedinLogo from 'assets/LinkedinLogo.png';
+import GithubLogo from 'assets/Githublogo.png';
 
-import styles from "./Style.module.scss";
+import styles from './Style.module.scss';
 
-const Footer = props => {
+const Footer = (props) => {
   const backToTop = () => window.scrollTo(0, 0);
 
   const hoverScale = 1.2;
@@ -18,27 +19,41 @@ const Footer = props => {
     <div className={styles.footerWrapper}>
       <div className={styles.tooltip}>
         <span className={styles.tooltiptext}>Email Aidan</span>
-        <a href="mailto:dev@aidanlowson.com">
+        <a href='mailto:dev@aidanlowson.com'>
           <motion.div
             whileHover={{ scale: hoverScale }}
             whileTap={{ scale: tapScale }}
           >
-            <EmailIcon fontSize="large" />
+            <EmailIcon fontSize='large' />
           </motion.div>
+        </a>
+      </div>
+      <div className={styles.tooltip}>
+        <span className={styles.tooltiptext}>Aidans Github profile</span>
+        <a href='https://github.com/AidanLDev'>
+          <motion.img
+            src={GithubLogo}
+            alt='Github Logo'
+            width='50'
+            height='50'
+            whileHover={{ scale: hoverScale }}
+            whileTap={{ scale: tapScale }}
+            className={styles.socialMedia}
+          />
         </a>
       </div>
       <div className={styles.tooltip}>
         <span className={styles.tooltiptext}>Linkedin Profile</span>
         <a
-          href="https://www.linkedin.com/in/aidanlowson1/"
-          rel="noopener noreferrer"
-          target="_blank"
+          href='https://www.linkedin.com/in/aidanlowson1/'
+          rel='noopener noreferrer'
+          target='_blank'
         >
           <motion.img
             src={LinkedinLogo}
-            alt="LinkedIn Logo"
-            width="50"
-            height="50"
+            alt='LinkedIn Logo'
+            width='50'
+            height='50'
             whileHover={{ scale: hoverScale }}
             whileTap={{ scale: tapScale }}
             className={styles.socialMedia}
@@ -55,7 +70,7 @@ const Footer = props => {
         <Icon
           path={mdiChevronUp}
           size={1}
-          color={"white"}
+          color={'white'}
           className={styles.mobileOnly}
         />
       </motion.div>
