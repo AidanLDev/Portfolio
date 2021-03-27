c45ca00eee7b36e14a98ce5f5d9469a1717d4ad6
Aidan <aidanlowson@gmail.com>
2021-03-27 15:27:46 +0000

    Reformatting test file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./containers/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './containers/App';
 
-import "./index.css";
+import './index.css';
 
 const app = (
   <BrowserRouter>
@@ -11,4 +11,4 @@ const app = (
   </BrowserRouter>
 );
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(app, document.getElementById('root'));
