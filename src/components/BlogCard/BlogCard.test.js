2bd1ad65c1620e66e3ba007e641d8c3adbf7e770
Aidan <aidansoftwaredev@gmail.com>
2021-04-27 22:45:50 +0100

    More tsts

--- src/components/BlogCard/BlogCard.test.js
+++ src/components/BlogCard/BlogCard.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BlogCard from './BlogCard';
 
-test('Renders BlogCard Component', () => {
+describe('<BlogCard />', () => {
   const component = shallow(
     <BlogCard title='About Aidans Life' description='once upon a time...' />
   );
-  expect(component).toMatchSnapshot();
+  test('Renders BlogCard Component', () => {
+    expect(component).toMatchSnapshot();
+  });
 });
