9a97ea5e0ec5c08bf9340eec23eb55c7faf54380
Aidan Lowson <aidan.lowson@contactpartners.com>
2021-04-28 11:53:58 +0100

    Test WIP

--- src/components/BlogCard/BlogCard.test.js
+++ src/components/BlogCard/BlogCard.test.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import BlogCard from './BlogCard';
+import styles from './Style.module.scss';
 
 describe('<BlogCard />', () => {
+  const titleProp = 'About Aidans Life';
+  const descriptionProp = 'once upon a time...';
   const component = shallow(
-    <BlogCard title='About Aidans Life' description='once upon a time...' />
+    <BlogCard title={titleProp} description={descriptionProp} />
   );
   test('Renders BlogCard Component', () => {
     expect(component).toMatchSnapshot();
   });
+
+  // https://enzymejs.github.io/enzyme/docs/api/ShallowWrapper/contains.html
+  // it('Contains a title and paragraph', () => {
+  //   expect(component.contains(<div className={styles.blogCardContainer}>
+  //     <h1>{titleProp}</h1>
+  //     <p>{descriptionProp}</p>
+  //   </div>).toEqual(true))
+  // })
 });
