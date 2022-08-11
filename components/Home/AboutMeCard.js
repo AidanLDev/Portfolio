29d1759d3591b526c0c0034450d221b22b5ea7ee
AidanLDev <dev@aidanlowson.com>
2022-08-11 20:42:41 +0100

    Card updats

--- components/Home/AboutMeCard.js
+++ components/Home/AboutMeCard.js
@@ -1,20 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Box, Image, Text, Heading, Center } from '@chakra-ui/react'
+import { Box, Divider, Text, Heading, Center } from '@chakra-ui/react'
 import React from 'react'
 
 export default function AboutMeCard() {
   return (
     <Box w="400px" rounded="20px" overflow="hidden" boxShadow="md">
-      {/* Nice bg image */}
-      <Box p={5}>
+      <Box p={5} backgroundImage={`/images/spaceBgS.jpg`} h="450px">
         <Center>
-          <Heading className="heading">About Me</Heading>
+          <Heading className="cardHeading">About Me</Heading>
         </Center>
+        <Divider marginBottom="24px" />
         <Text>
           I'm a Frontend developer from the UK who loves creating websites.
           Hobbies wise I play the guitar, enjoy both learning and experiencing
           new things such as a language, both programming and human ones,
-          traveling around and keeping fit usually by running.
+          traveling around and keeping active.
         </Text>
         {/* Links to GH, email and linkedIn */}
       </Box>
