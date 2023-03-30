9fb8691dab7205f5ea2f70e25b16ee6d6c15d348
Aidan <aidan.lowson@davies-group.com>
2023-03-30 15:40:42 +0100

    Finishing links

--- components/Home/AboutMeCard.js
+++ components/Home/AboutMeCard.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Box, Text, Heading, Center, VStack, Image } from '@chakra-ui/react'
+import { Box, Text, Heading, Center, Image } from '@chakra-ui/react'
 import React from 'react'
 import ImageLink from './ImageLink'
 
@@ -26,21 +26,43 @@ export default function AboutMeCard() {
           <Heading className="cardHeading">DevOps Engineer</Heading>
         </Center>
       </Box>
-      <Box p={5} h="255px" className="detailsBox" position="relative">
+      <Box p={5} h="300px" className="detailsBox" position="relative">
         <Text className="cardText">
           Full time DevOps technician, aspiring freelancer. Hover for links and stuff
         </Text>
-        <VStack className="cardLinks" transition="all 1.5s ease-in-out">
+        <Box className="cardLinks" transition="all 1.5s ease-in-out" pos="relative" top="-132px">
           <ImageLink
-            alt="GitHub Image"
+            alt="GitHub"
             link="https://github.com/AidanLDev"
-            imagePath="/images/Logos/GithubLogo.png"
+            imagePath="/images/Logos/github-512.webp"
           />
-          {/* Double A Team */}
-          {/* Email */}
-          {/* LinkedIn */}
+          <ImageLink
+            alt="E-Mail me"
+            link="mailto:dev@aidanlowson.com"
+            imagePath="/images/Logos/round-blue-mail-icon.png"
+          />
+          <ImageLink
+            alt="LinkedIn"
+            link="https://www.linkedin.com/in/aidanlowson1/"
+            imagePath="/images/Logos/LinkedIn_icon_circle.svg.png"
+          />
+          <ImageLink
+            alt="Twitter"
+            link="https://twitter.com/AidanL94"
+            imagePath="/images/Logos/twitter-circle-2.png"
+          />
+          <ImageLink
+            alt="Instagram"
+            link="https://www.instagram.com/lowsonaidan/"
+            imagePath="/images/Logos/InstagramCircle.webp"
+          />
+          <ImageLink
+            alt="Personal Blog"
+            link="https://doubleateam.co.uk"
+            imagePath="/images/Logos/blog-icon.png"
+          />
 
-        </VStack>
+        </Box>
       </Box>
     </Box>
   )
--- components/Home/ImageLink.js
+++ components/Home/ImageLink.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Image } from '@chakra-ui/react'
+import { Image, Tooltip } from '@chakra-ui/react'
 
 export default function ImageLink({ link, imagePath, alt }) {
   return (
     <a href={link} rel="noreferrer" target="_blank">
-      <Image alt={alt} src={imagePath} cursor="pointer" />
+      <Tooltip label={alt} placement='auto-start'>
+        <Image w="50px" alt={alt} src={imagePath} cursor="pointer" />
+      </Tooltip>
     </a>
   )
 }
