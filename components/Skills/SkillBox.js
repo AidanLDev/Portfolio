3d1821780fe63887bd32b59a40153c74240d40c1
Aidan <aidanlowson@gmail.com>
2023-05-11 20:04:03 +0100

    Animation

--- components/Skills/SkillBox.js
+++ components/Skills/SkillBox.js
@@ -1,13 +1,21 @@
 import { Image } from '@chakra-ui/image'
 import { Text } from '@chakra-ui/layout'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 import styles from './styles.module.scss'
 
 export default function SkillBox({ logo, name }) {
   return (
     <div className={styles.skillsBox}>
-      <Image width={50} height={50} src={logo} alt={name} />
+      <Image
+        as={motion.img}
+        width={50}
+        height={50}
+        src={logo}
+        alt={name}
+        whileHover={{ scale: 1.1 }}
+      />
       <Text>{name}</Text>
     </div>
   )
