54144993e473904e5277039b795222e330fb2459
Aidan <aidanlowson@gmail.com>
2023-05-12 17:33:09 +0100

    Project card stuff

--- components/Projects/ProjectCard.js
+++ components/Projects/ProjectCard.js
@@ -3,16 +3,20 @@ import React from 'react'
 import styles from './style.module.scss'
 import { Image, Tooltip } from '@chakra-ui/react'
 
+import { motion } from 'framer-motion'
+
 export default function ProjectCard({ img, link, tooltip }) {
   return (
     <div className={styles.projectCard}>
       <a href={link} rel="noreferrer" target="_blank">
         <Tooltip label={tooltip}>
           <Image
+            as={motion.img}
             alt={tooltip}
             src={`/images/Projects/${img}`}
             h={240}
             width="100%"
+            whileHover={{ scale: 1.1 }}
           />
         </Tooltip>
       </a>
--- components/Projects/ProjectContainer.js
+++ components/Projects/ProjectContainer.js
@@ -5,22 +5,43 @@ import style from './style.module.scss'
 import ProjectCard from './ProjectCard'
 
 const projects = [
-  { img: 'doubleATeam.png', tooltip: 'The Double A Team blog', link: 'https://blog.aidanlowson.com' },
-  { img: 'MusicLibraryProject.png', tooltip: 'Music library search' },
-  { img: 'ReactBootstrapCityGuide.png', tooltip: 'React city guide made with Bootstrap' },
-  { img: 'fizzBuzz.png', tooltip: 'FizzBuzz JS edition' }
+  {
+    img: 'doubleATeam.png',
+    tooltip: 'The Double A Team blog',
+    link: 'https://blog.aidanlowson.com',
+  },
+  {
+    img: 'MusicLibraryProject.png',
+    tooltip: 'Music library search',
+    link: 'https://song-search-web-search.onrender.com/',
+  },
+  {
+    img: 'ReactBootstrapCityGuide.png',
+    tooltip: 'React city guide made with Bootstrap',
+    link: 'http://react-bootstrap-city-guide.s3-website.eu-west-2.amazonaws.com/',
+  },
+  {
+    img: 'fizzBuzz.png',
+    tooltip: 'FizzBuzz JS edition',
+    link: 'http://fizz-buzz.s3-website.eu-west-2.amazonaws.com/',
+  },
 ]
 
 export default function ProjectContainer() {
   return (
     <div className={`${style.projectContainer}`}>
       <Center>
-        <Heading p='2rem 0'>
-          Projects
-        </Heading>
+        <Heading p="2rem 0">Projects</Heading>
       </Center>
-      <Box display='flex' flexWrap='wrap'>
-        {projects.map((project, idx) => <ProjectCard key={`${project.tooltip}__${idx}`} img={project.img} tooltip={project.tooltip} link={project.link} />)}
+      <Box display="flex" flexWrap="wrap">
+        {projects.map((project, idx) => (
+          <ProjectCard
+            key={`${project.tooltip}__${idx}`}
+            img={project.img}
+            tooltip={project.tooltip}
+            link={project.link}
+          />
+        ))}
       </Box>
     </div>
   )
