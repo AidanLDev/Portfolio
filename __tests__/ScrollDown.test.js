89924869175106d3d602522b37a0999dd2ac5934
AidanLDev <dev@aidanlowson.com>
2023-07-15 13:02:20 +0100

    Playing with jest config

--- __tests__/ScrollDown.test.js
+++ __tests__/ScrollDown.test.js
@@ -1,4 +1,6 @@
-import ScrollDown from './ScrollDown'
+import ScrollDown from '../components/Home/ScrollDown'
+import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 describe('ScrollDown component', () => {
