18fc239819cd9c92d76e91e622d9fc1834804eb2
Aidan <aidan.lowson@contactpartners.com>
2023-08-10 20:20:54 +0100

    Tag test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -2,8 +2,14 @@ describe('Navigation', () => {
   it('Should navigate to available links', () => {
     cy.visit('http://localhost:3000/')
 
-    cy.contains('Blogs').click()
+    // cy.contains('Blogs').click()
     cy.contains('AL').click()
     cy.url().should('include', '/')
+  })
+})
+
+describe('Display tags correctly', () => {
+  it('Displays Body opening and closing tags', () => {
+    cy.get('.top-tags').contains('body')
   })
 })
