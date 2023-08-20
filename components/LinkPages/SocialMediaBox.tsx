7b24cba11434f9c8c986b88832e09c13d68d9351
Aidan <dev@aidanlowson.com>
2023-08-20 13:11:59 +0100

    Format it better

--- components/LinkPages/SocialMediaBox.tsx
+++ components/LinkPages/SocialMediaBox.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Text, Flex, Center } from '@chakra-ui/react'
 
-export default function SocialMediaBox() {
+import styles from './style.module.scss'
+
+interface SocialMediaBoxProps {
+  backgroundImage: string
+  tag: string
+  link: string
+}
+
+export default function SocialMediaBox(props: SocialMediaBoxProps) {
   return (
-    <Box
-      backgroundImage="/images/Logos/InstagramCircle.webp"
-      backgroundSize="contain"
-      w={125}
-      h={125}
-      borderRadius="100px"
-      cursor='pointer'
-    ></Box>
+    <a href="https://www.instagram.com/mynameisarni/" target="_blank">
+      <Flex
+        backgroundImage={props.backgroundImage}
+        backgroundSize="contain"
+        w={175}
+        h={175}
+        borderRadius="100px"
+        cursor="pointer"
+        className={styles.socialMediaBox}
+      >
+        <Center>
+          <Text>{props.tag}</Text>
+        </Center>
+      </Flex>
+    </a>
   )
 }
--- pages/links-arni/index.tsx
+++ pages/links-arni/index.tsx
@@ -4,16 +4,31 @@ import { Box, Center, Flex, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 
 import SocialMediaBox from '../../components/LinkPages/SocialMediaBox'
+import styles from './style.module.scss'
+
+interface SocialMediasObject {
+  img: string
+  tag: string
+  link: string
+}
+
+const socialMedias: SocialMediasObject[] = [
+  {
+    img: '/images/Logos/InstagramCircle.webp',
+    tag: '@mynameisarni',
+    link: 'https://www.instagram.com/mynameisarni/'
+  },
+]
 
 export default function ArniLinks() {
   return (
     <Box p="64px 16px 32px">
-      <Center>
+      <Center className={styles.avatarImg}>
         <Image
           alt="Arni Rianis avatar/profile picture"
           src="/images/arni-avatar.webp"
-          width={125}
-          height={125}
+          width={175}
+          height={175}
         />
       </Center>
       <Flex justify="center" p="12px 0">
@@ -27,7 +42,13 @@ export default function ArniLinks() {
         />
       </Flex>
       <Flex gap="22px" justify="space-around">
-        <SocialMediaBox />
+        {socialMedias.map((social, idx) => (
+          <SocialMediaBox
+            backgroundImage={social.img}
+            tag={social.tag}
+            key={`${social.img}__${idx}`}
+          />
+        ))}
       </Flex>
     </Box>
   )
