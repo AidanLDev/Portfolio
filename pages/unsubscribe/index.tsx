a0906198b152f0ea49005f4b586f6183a03d0c28
Aidan Lowson <dev@aidanlowson.com>
2024-08-19 09:01:21 +0100

    Styling up the page a little

--- pages/unsubscribe/index.tsx
+++ pages/unsubscribe/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Input, Text } from '@chakra-ui/react'
+import { Box, Button, Heading, Input, Text } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import styles from './styles.module.scss'
 
 export default function Unsubscribe() {
   const [email, setEmail] = useState('')
@@ -17,9 +18,12 @@ export default function Unsubscribe() {
     })
   }
   return (
-    <div>
-      <h1>Unsubscribe</h1>
-      <p>Press the button below and double check</p>
+    <div className={styles.unsubscribeContainer}>
+      <Heading >Unsubscribe</Heading>
+      <p>
+        Enter your email and press un-subscribe to be removed from the mailing
+        list
+      </p>
       <Box>
         <Text>Email:</Text>
         <Input
