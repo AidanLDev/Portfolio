d5fc1865031497206e836336ca6dfa6c23b4a9ca
Aidan Lowson <aidan.lowson@processvision.com>
2024-08-19 10:55:31 +0100

    Adding env to sst.config

--- sst.config.ts
+++ sst.config.ts
@@ -14,6 +14,10 @@ export default $config({
         name: 'aidanlowson.com',
         cert: 'arn:aws:acm:us-east-1:136597286325:certificate/7634b5c3-5a42-46e4-9e83-c1ee57b6f1c2',
       },
+      environment: {
+        MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID!,
+        MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY!,
+      },
     })
   },
 })
