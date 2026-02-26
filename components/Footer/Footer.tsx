b0b2e133bf73b1c1f12c5c5b19d9f35c92f4914e
AidanLDev <dev@aidanlowson.com>
2026-02-26 09:30:11 +0000

    Updating the footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,10 +5,17 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
-      <Link href="#nav-bar">
-        <span>Back to top</span>
-        <Image alt="Chevron up icon" width={24} height={24} src="/icons/chevronUpIcon.svg" />
-      </Link>
+      <span>Made by Aidan Lowson, thanks for visiting</span>
+      <div className={styles.linkSection}>
+        <Link href="#nav-bar">
+          <Image
+            alt="Chevron up icon"
+            width={24}
+            height={24}
+            src="/icons/chevronUpIcon.svg"
+          />
+        </Link>
+      </div>
     </footer>
   );
 }
